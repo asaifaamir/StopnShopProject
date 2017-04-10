8be4978b92a447cb6c958f2e50104645ff5b6fd5
asaifbutt <asaifaamir@hotmail.com>
2017-04-10 00:05:56 -0400

    Create the registration panel form and login panel form in view package

--- src/view/loginPanel.java
+++ src/view/loginPanel.java
@@ -1,7 +1,35 @@
 package view;
 
+import javax.swing.*;
+
 /**
  * Created by asaifbutt on 4/6/17.
  */
 public class loginPanel {
+    private JPanel loginPanel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton registerButton;
+    private JButton loginButton;
+
+    public loginPanel() {
+
+
+    }
+
+
+    public static void main(String [] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setContentPane(new loginPanel().loginPanel);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+
+
+    }
+
 }
+
--- src/view/registrationPanel.java
+++ src/view/registrationPanel.java
@@ -1,7 +1,36 @@
 package view;
 
+import javax.swing.*;
+
 /**
  * Created by asaifbutt on 4/6/17.
  */
 public class registrationPanel {
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JPanel registration;
+    private JButton registerButton;
+
+    public registrationPanel()
+    {
+
+    }
+
+    public static void main(String [] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setContentPane(new registrationPanel().registration);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+
+
+    }
 }
