fb69e6ae9e5ee0709faa11f0addbce1f1b61502d
toneloc166 <antoniozuniga31@yahoo.com>
2017-04-15 18:01:23 -0400

    Add files via upload

---
+++ src/controller/AccountListWrapper.java
@@ -0,0 +1,15 @@
+package ShoeCart_Interface;
+
+import Model.Account;
+/**
+ *
+ * @author Antonio Zuniga
+ */
+public interface AccountListWrapper
+{ 
+  public void insert(Account data);
+  public void remove(String userName);
+  public Account retrieve(String userName);
+  public boolean search(String userName);
+  public int size();
+}
